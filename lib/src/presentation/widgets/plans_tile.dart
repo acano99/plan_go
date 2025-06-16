1f98285e6105c77807d455d8f8d3710d017cac62
acano99 <abrahan.jcano99@gmail.com>
2025-06-16 17:42:15 -0400

    feat: widget de la pagina gome para mostrar los servicios

---
+++ lib/src/presentation/widgets/plans_tile.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:plan_go/src/data/models/plan.dart';
+
+class PlansTile extends StatelessWidget {
+  const PlansTile({super.key, required this.plan, required this.onTap});
+
+  final Plan plan;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusGeometry.circular(20),
+          ),
+          child: Padding(
+            padding: EdgeInsetsGeometry.symmetric(horizontal: 16, vertical: 12),
+            child: Row(
+              spacing: 24,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 4,
+                    children: [
+                      Text(
+                        plan.name,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(plan.label, style: TextStyle(fontSize: 14)),
+                    ],
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadiusGeometry.circular(12),
+                  child: Image.asset(
+                    plan.image,
+                    width: 100,
+                    height: 80,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
