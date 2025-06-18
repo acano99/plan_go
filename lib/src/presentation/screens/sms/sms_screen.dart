1c25a95474a6c2a4b5277e3c332c9105876b249b
acano99 <abrahan.jcano99@gmail.com>
2025-06-17 22:01:49 -0400

    feat: actualizacion menor

--- lib/src/presentation/screens/sms/sms_screen.dart
+++ lib/src/presentation/screens/sms/sms_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:plan_go/src/data/default/sms_list.dart';
-import 'package:plan_go/src/domain/services/ussd_service.dart';
+import 'package:plan_go/src/domain/use_cases/send_ussd_use_case.dart';
 import 'package:plan_go/src/presentation/widgets/sms_tile.dart';
 
 class SmsScreen extends StatelessWidget {
@@ -19,22 +19,7 @@ class SmsScreen extends StatelessWidget {
         itemCount: smsList.length,
         itemBuilder: (context, index) => SmsTile(
           sms: smsList[index],
-          onTap: (ussd) async {
-            try {
-              final response = await SendUssdService.sendUssd(ussd);
-              if (context.mounted) {
-                ScaffoldMessenger.of(
-                  context,
-                ).showSnackBar(SnackBar(content: Text(response)));
-              }
-            } catch (e) {
-              if (context.mounted) {
-                ScaffoldMessenger.of(
-                  context,
-                ).showSnackBar(SnackBar(content: Text(e.toString())));
-              }
-            }
-          },
+          onTap: (ussd) async => await SendUssdUseCase.invoke(context, ussd),
         ),
       ),
     );
