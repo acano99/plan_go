1a4b3ceddcd859808c6cdcc9361ef824861e69d6
acano99 <abrahan.jcano99@gmail.com>
2025-06-17 20:46:44 -0400

    feat: agregado el tema y funcionalidad para que se establezca el tema en funcion del sistema

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:plan_go/src/config/router/router.dart';
+import 'package:plan_go/src/config/theme/theme.dart';
 
-void main() {
+void main() async {
   runApp(DevicePreview(enabled: !kReleaseMode, builder: (context) => MyApp()));
 }
 
@@ -12,18 +13,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final brightness = MediaQuery.platformBrightnessOf(context);
+
     return MaterialApp(
       useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-          brightness: Brightness.dark,
-        ),
-      ),
+      theme: brightness == Brightness.dark
+          ? MaterialTheme(TextTheme()).dark()
+          : MaterialTheme(TextTheme()).light(),
       routes: AppRouter().getRoutes(context),
       initialRoute: AppRouter.home,
     );
