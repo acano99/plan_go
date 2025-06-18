071fddc9099a8a51d80f5ea6e9fbbe91be3e8015
acano99 <abrahan.jcano99@gmail.com>
2025-06-17 20:48:56 -0400

    feat: listas con los valores por defecto

---
+++ lib/src/data/default/consultations_list.dart
@@ -0,0 +1,46 @@
+import 'package:plan_go/src/data/models/consultation.dart';
+
+final consultationsList = <Consultation>[
+  Consultation(
+    'Saldo',
+    'Consulta general de saldo, datos, sms y voz.',
+    '*222#',
+    'assets/images/consult_plan.png',
+  ),
+  Consultation(
+    'Saldo Corporativo',
+    'Consulta general de saldo, datos, sms y voz.',
+    '*111#',
+    'assets/images/consult_plan.png',
+  ),
+  Consultation(
+    'Datos',
+    'Consulta tu plan de datos.',
+    '*222*328#',
+    'assets/images/consult_plan.png',
+  ),
+  Consultation(
+    'Bonos y Planes USD',
+    'Consulta tus bonos y planes USD.',
+    '*222*266#',
+    'assets/images/consult_plan.png',
+  ),
+  Consultation(
+    'Voz',
+    'Consulta tus minutos.',
+    '*222*869#',
+    'assets/images/consult_plan.png',
+  ),
+  Consultation(
+    'SMS',
+    'Consulta tus sms.',
+    '*222*767#',
+    'assets/images/consult_plan.png',
+  ),
+  Consultation(
+    'Recargas Nacionales',
+    'Consulta el estado de tus recargas nacionales.',
+    '*222*732#',
+    'assets/images/consult_plan.png',
+  ),
+];
---
+++ lib/src/data/default/sms_list.dart
@@ -0,0 +1,13 @@
+import 'package:plan_go/src/data/models/sms.dart';
+
+final smsList = <Sms>[
+  Sms('Plan 20sms', '15 Cup', 'assets/images/mobile_sms_plan.png', '*133*2*1#'),
+  Sms('Plan 50sms', '30 Cup', 'assets/images/mobile_sms_plan.png', '*133*2*2#'),
+  Sms('Plan 90sms', '50 Cup', 'assets/images/mobile_sms_plan.png', '*133*2*2#'),
+  Sms(
+    'Plan 120SMS',
+    '60 Cup',
+    'assets/images/mobile_sms_plan.png',
+    '*133*2*4#',
+  ),
+];
