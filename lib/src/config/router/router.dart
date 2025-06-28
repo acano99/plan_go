4ca2375ba764840f51d2ba7df3e416cebad0dcd2
acano99 <abrahan.jcano99@gmail.com>
2025-06-28 17:36:45 -0400

    update: actualizacion de las rutas

--- lib/src/config/router/router.dart
+++ lib/src/config/router/router.dart
@@ -4,9 +4,10 @@ import 'package:plan_go/src/presentation/screens/data/data_screen.dart';
 import 'package:plan_go/src/presentation/screens/settings/settings_screen.dart';
 import 'package:plan_go/src/presentation/screens/sms/sms_screen.dart';
 import 'package:plan_go/src/presentation/screens/start/start_screen.dart';
+import 'package:plan_go/src/presentation/screens/transfer/transfer_screen.dart';
 import 'package:plan_go/src/presentation/screens/voice/voice_screen.dart';
 
-import '../../presentation/screens/plans/home_screen.dart';
+import '../../presentation/screens/plans/plan_screen.dart';
 
 class AppRouter {
   static const String start = '/';
@@ -16,14 +17,16 @@ class AppRouter {
   static const String voice = '/voice';
   static const String consultation = '/consultation';
   static const String settings = '/settings';
+  static const String transfer = '/transfer';
 
   Map<String, WidgetBuilder> getRoutes(BuildContext context) => {
     AppRouter.start: (context) => StartScreen(),
-    AppRouter.home: (context) => HomeScreen(),
+    AppRouter.home: (context) => PlanScreen(),
     AppRouter.consultation: (context) => ConsultationScreen(),
     AppRouter.data: (context) => DataScreen(),
     AppRouter.voice: (context) => VoiceScreen(),
     AppRouter.sms: (context) => SmsScreen(),
     AppRouter.settings: (context) => SettingsScreen(),
+    AppRouter.transfer: (context) => TransferScreen(),
   };
 }
